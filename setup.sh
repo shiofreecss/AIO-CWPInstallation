961eb36ee36356f6ee4c57874636df82ad92ab70
Katanuki Satoshi <katanukisatoshi@gmail.com>
2024-10-15 20:40:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 yum -y update
+#fix https://forum.centos-webpanel.com/csf-firewall/firewall-does-not-get-enabled-in-centos8-install-of-cwp/
 yum -y install nano wget iptables
 cd /usr/local/src
 wget http://centos-webpanel.com/cwp-el9-latest
