72aeae0754e2a754afb6a6b9188f9237abdc0a1c
ShioDev | 塩 <nhvu.dalat@gmail.com>
2025-04-16 08:03:14 +0700

    Update setup.sh
    revert to 8 for stabler

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 yum -y update
 yum -y install nano wget iptables
 cd /usr/local/src
-wget http://centos-webpanel.com/cwp-el9-latest
-sh cwp-el9-latest
+wget http://centos-webpanel.com/cwp-el8-latest
+sh cwp-el8-latest
 reboot
